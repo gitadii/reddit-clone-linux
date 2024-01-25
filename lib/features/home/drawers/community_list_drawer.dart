813a1396c0ffc50d550cfe06ca81ad92fc7927f3
gitadii <aduvishwakarma123@gmail.com>
2024-01-25 11:32:06 +0530

    designed homeScreen and added communityListDrawer

---
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CommunityListDrawer extends ConsumerWidget {
+  const CommunityListDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Create a community'),
+              leading: Icon(Icons.add),
+            )
+          ],
+        ) 
+      ),
+    );
+  }
+}
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:redditclone_linux/features/auth/controller/auth_controller.dart';
+import 'package:redditclone_linux/features/home/drawers/community_list_drawer.dart';
 
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
+  void displayDrawer( BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
-      appBar: AppBar(leading: const Icon(Icons.menu),),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+            onPressed: () => displayDrawer(context),
+            );
+          }
+        ),
+        title: const Text('Home', 
+        style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search),),
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+              radius: 13.0,
+            ),
+            onPressed: () {},
+          )
+        ],
+        ),
       body: Center(
-        child : Text(user.name),
+        child : Text(user.karma.toString()),
       ),
+      drawer: CommunityListDrawer(),
     );
   }
 }
